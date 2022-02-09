2fd67c6ab016641249b225f6df34d28cfbba6aa2
Michal Prihoda <michal@prihoda.net>
2022-02-09 15:52:34 +0100

    Remove the dashed box.

--- app/src/main/scala/cz/e_bs/cmi/mdr/pdb/app/components/Layout.scala
+++ app/src/main/scala/cz/e_bs/cmi/mdr/pdb/app/components/Layout.scala
@@ -21,15 +21,7 @@ def MainSection(mods: Modifier[HtmlElement]*): HtmlElement =
   main(
     div(
       cls := "max-w-7xl mx-auto py-6 sm:px-6 lg:px-8",
-      // <!-- Replace with your content -->
-      div(
-        cls := "px-4 py-4 sm:px-0",
-        div(
-          cls := "border-4 border-dashed border-gray-200 rounded-lg h-96",
-          mods
-        )
-      )
-      // <!-- /End replace -->
+      mods
     )
   )
 
