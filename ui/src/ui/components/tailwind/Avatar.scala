be82913525c069caf7ef786c8100761400d17c50
Michal Prihoda <michal@prihoda.net>
2022-03-19 16:39:07 +0100

    De-macro avatar, not working.

--- ui/src/ui/components/tailwind/Avatar.scala
+++ ui/src/ui/components/tailwind/Avatar.scala
@@ -3,31 +3,36 @@ package works.iterative.ui.components.tailwind
 import CustomAttrs.ariaHidden
 import com.raquo.laminar.api.L.{*, given}
 import com.raquo.domtypes.generic.codecs.BooleanAsTrueFalseStringCodec
-import works.iterative.ui.components.tailwind.Macros
 
-// TODO: render icon or picture based on img signal
+// TODO: macros for size
 class Avatar($avatarImg: Signal[Option[String]]):
-  inline def avatarPlaceholder(size: Int): HtmlElement =
+  inline def avatarPlaceholder(
+      extraClasses: String,
+      iconClasses: String
+  ): HtmlElement =
     div(
       cls := s"rounded-full text-indigo-200 bg-indigo-500 flex items-center justify-center",
-      cls := Macros.size(size),
-      Icons.outline.user(Macros.size(size - 2))
+      cls := extraClasses,
+      Icons.outline.user(iconClasses)
     )
 
-  inline def avatarImage(size: Int): Signal[HtmlElement] =
+  inline def avatarImage(
+      extraClasses: String,
+      iconClasses: String
+  ): Signal[HtmlElement] =
     $avatarImg.split(_ => ())((_, _, $url) =>
       img(
         cls := s"rounded-full",
-        cls := Macros.size(size),
+        cls := extraClasses,
         src <-- $url,
         alt := ""
       )
-    ).map(_.getOrElse(avatarPlaceholder(size)))
+    ).map(_.getOrElse(avatarPlaceholder(extraClasses, iconClasses)))
 
-  inline def avatar(size: Int): HtmlElement =
+  inline def avatar(extraClasses: String, iconClasses: String): HtmlElement =
     div(
       cls := "relative",
-      child <-- avatarImage(size),
+      child <-- avatarImage(extraClasses, iconClasses),
       span(
         cls := "absolute inset-0 shadow-inner rounded-full",
         ariaHidden := true
