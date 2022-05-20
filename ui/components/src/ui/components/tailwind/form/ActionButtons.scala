5c5666c81329aecc66b30fd1064e90d683afa97b
Michal Prihoda <michal@prihoda.net>
2022-05-20 19:00:50 +0200

    Fix validation availability.

--- ui/components/src/ui/components/tailwind/form/ActionButtons.scala
+++ ui/components/src/ui/components/tailwind/form/ActionButtons.scala
@@ -29,7 +29,9 @@ object ActionButtons:
             cls(
               "bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             ),
-            ctx.messages(s"action.$name.title"),
+            ctx
+              .messages(s"action.$name.title")
+              .getOrElse(s"action.$name.title"),
             onClick.mapTo(action) --> actions
           )
         }
