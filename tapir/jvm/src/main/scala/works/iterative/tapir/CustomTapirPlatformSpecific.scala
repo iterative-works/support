3bb3878d0d0fc13b3cc64a03dbd4a07887c519ca
Michal Prihoda <michal@prihoda.net>
2022-05-28 01:27:27 +0200

    Mapping update

--- tapir/jvm/src/main/scala/works/iterative/tapir/CustomTapirPlatformSpecific.scala
+++ tapir/jvm/src/main/scala/works/iterative/tapir/CustomTapirPlatformSpecific.scala
@@ -37,9 +37,11 @@ trait CustomTapirPlatformSpecific extends ZTapir with SttpClientInterpreter:
     val req = toRequestThrowErrors(endpoint, baseUri.toUri)
     req(_)
       .followRedirects(false)
+      // TODO: set auth header from client side somehow
       .header(
         "Cookie",
-        "PLAY_SESSION=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImVycm9yX3JlZGlyZWN0IjoiaHR0cDovL2xvY2FsaG9zdDo5MDAwL21kci9hdXRoX2Vycm9yIiwiY3NyZlRva2VuIjoiNGUwNzk1M2Q4NjU3MmZkMGNhNTY5NTM0NTVhMDJiNmE4Y2RhZDc5OS0xNjI0ODk3NjQ1MTM0LWQzMmRlZmIyZDJhY2M0ZmM4YzI4ZTQyMCIsImFwcF9yZWRpcmVjdCI6Imh0dHA6Ly9sb2NhbGhvc3Q6OTAwMC9tZHIvYXBwIiwic3RhdGUiOiI5MWJiMjU5ZmMyZWMwNjUwODQxYWU2YTciLCJvc29ibmlDaXNsbyI6Ijk5OTEyMyIsInJvbGUiOiJhZG1pbmlzdHJhdG9yIn0sIm5iZiI6MTYyNDg5NzY0NSwiaWF0IjoxNjI0ODk3NjQ1fQ._P_wODFg1mbWxVkkLLCo74bCnsmcBFgKvbVECCnnrp0; _dd_s=logs=1&id=c63a44e3-02db-47c6-9234-ee546ce72096&created=1653641211408&expire=1653645413320; pac4jCsrfToken=efdaefd57128407197c3912d76700401; pac4jSessionId=3723ae2d-6d41-4ce1-8eb5-1e9c385e19ce"
+        // "PLAY_SESSION=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7InN0YXRlIjoiZDkxNGU4ZmRmZjI2ZmQ5ZDdmOGI2MzhiIiwiYXBwX3JlZGlyZWN0IjoiaHR0cHM6Ly90YzE2My5jbWkuY3ovbWRyL2FwcCIsImVycm9yX3JlZGlyZWN0IjoiaHR0cHM6Ly90YzE2My5jbWkuY3ovbWRyL2F1dGhfZXJyb3IifSwibmJmIjoxNjUzMzI2NDY5LCJpYXQiOjE2NTMzMjY0Njl9.kiLc-hdyzmX9m3i63K2qnIxlKtGQ_DeqoSWuGgY0pco; pac4jSessionId=03b38525-4c5c-42df-974e-a7db3e42586b; _dd_s=logs=1&id=ab704848-2128-4011-a53e-5305b434e172&created=1653690073090&expire=1653690975966"
+        "pac4jSessionId=8c583af3-dc10-46d9-8c44-a53a2c0f3e18; _dd_s=logs=1&id=36d46f53-9cee-4526-9da1-d9534d310584&created=1653688998526&expire=1653691476217"
       )
       .send(backend)
       .map(_.body)
