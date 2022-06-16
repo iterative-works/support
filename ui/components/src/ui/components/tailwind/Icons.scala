cd9d4f6a2f6c27240c20348577162d3120e66244
Michal Prihoda <michal@prihoda.net>
2022-06-16 10:56:01 +0200

    Link on proof list in role crit list.

--- ui/components/src/ui/components/tailwind/Icons.scala
+++ ui/components/src/ui/components/tailwind/Icons.scala
@@ -183,6 +183,21 @@ object Icons:
         )
       )
 
+    inline def document(extraClasses: String): SvgElement =
+      svg(
+        cls(extraClasses),
+        fill := "none",
+        stroke := "currentColor",
+        viewBox := "0 0 24 24",
+        xmlns := "http://www.w3.org/2000/svg",
+        path(
+          strokeLineCap := "round",
+          strokeLineJoin := "round",
+          strokeWidth := "2",
+          d := "M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"
+        )
+      )
+
   end outline
 
   object solid:
