68bc00901954cde98ee781ecc9d8f5400fcf9840
Michal Prihoda <michal@prihoda.net>
2022-07-07 01:33:15 +0200

    Form submission and display done.

--- ui/components/src/ui/components/tailwind/Icons.scala
+++ ui/components/src/ui/components/tailwind/Icons.scala
@@ -382,6 +382,19 @@ object Icons:
         )
       )
 
+    inline def `dots-circle-horizontal`(extraClasses: String): SvgElement =
+      svg(
+        cls := extraClasses,
+        fill := "currentColor",
+        viewBox := "0 0 20 20",
+        xmlns := "http://www.w3.org/2000/svg",
+        path(
+          fillRule := "evenodd",
+          d := "M10 18a8 8 0 100-16 8 8 0 000 16zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z",
+          clipRule := "evenodd"
+        )
+      )
+
     inline def `x-circle`(extraClasses: String): SvgElement =
       svg(
         cls := extraClasses,
--- ui/components/src/ui/components/tailwind/StyleGuide.scala
+++ ui/components/src/ui/components/tailwind/StyleGuide.scala
@@ -29,18 +29,22 @@ object StyleGuide:
 
     override object button extends ButtonStyles:
       private def common(extra: String) =
-        s"inline-flex items-center px-4 py-2 $extra border rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        s"inline-flex items-center px-4 py-2 $extra border rounded-md shadow-sm text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-75"
       override val basic: String =
-        common("border-gray-300 text-gray-700 bg-white hover:bg-gray-50")
+        common(
+          "border-gray-300 text-gray-700 bg-white enabled:hover:bg-gray-50"
+        )
       override val primary: String =
         common(
-          "border-transparent text-white bg-indigo-600 hover:bg-indigo-700"
+          "border-transparent text-white bg-indigo-600 enabled:hover:bg-indigo-700"
         )
       override val secondary: String =
         common(
-          "border-gray-300 text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
+          "border-gray-300 text-indigo-700 bg-indigo-100 enabled:hover:bg-indigo-200"
         )
       override val positive: String =
-        common("border-gray-300 text-white bg-green-600 hover:bg-green-700")
+        common(
+          "border-gray-300 text-white bg-green-600 enabled:hover:bg-green-700"
+        )
       override val negative: String =
-        common("border-gray-300 text-white bg-red-600 hover:bg-red-700")
+        common("border-gray-300 text-white bg-red-600 enabled:hover:bg-red-700")
