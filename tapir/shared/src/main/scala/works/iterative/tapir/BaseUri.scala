50ca3634c5760e5447d834428dc9a30bfd97b085
Michal Prihoda <michal@prihoda.net>
2022-08-03 21:46:11 +0200

    App and development flow working.

--- tapir/shared/src/main/scala/works/iterative/tapir/BaseUri.scala
+++ tapir/shared/src/main/scala/works/iterative/tapir/BaseUri.scala
@@ -5,8 +5,5 @@ import sttp.model.Uri
 case class BaseUri(value: Option[Uri])
 
 object BaseUri:
-
-  def apply(optU: Option[Uri]): BaseUri = BaseUri(optU)
   def apply(u: Uri): BaseUri = BaseUri(Some(u))
-
   extension (v: BaseUri) def toUri: Option[Uri] = v.value
