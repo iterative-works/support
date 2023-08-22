ee83447cf697f896e9fe610a9f4fcd0afbfe670b
Michal Příhoda <michal@prihoda.net>
2023-08-22 13:23:42 +0200

    ScalaJS tests, separate scenarios modules

---
+++ ui/js/src/main/scala/works/iterative/ui/components/laminar/ZIOOwner.scala
@@ -0,0 +1,14 @@
+package works.iterative.ui.components.laminar
+
+import zio.*
+import com.raquo.airstream.ownership.Owner
+
+object ZIOOwner:
+  def acquire: UIO[ZIOOwner] = ZIO.succeed(new ZIOOwner)
+
+  val layer: ULayer[ZIOOwner] =
+    ZLayer.scoped(ZIO.acquireRelease(ZIOOwner.acquire)(_.release))
+
+class ZIOOwner extends Owner:
+  def release: UIO[Unit] =
+    ZIO.attempt(this.killSubscriptions()).ignore
