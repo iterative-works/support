5f8a4373226252096d1b2f429a4a5ab62057ef7e
Michal Příhoda <michal@prihoda.net>
2023-09-10 22:22:55 +0200

    PU1 hotovo.

--- ui/js/src/main/scala/works/iterative/ui/components/ZIODispatcher.scala
+++ ui/js/src/main/scala/works/iterative/ui/components/ZIODispatcher.scala
@@ -1,9 +1,12 @@
 package works.iterative.ui.components
 
 import zio.*
+import com.raquo.airstream.core.EventStream
+import works.iterative.ui.components.laminar.LaminarExtensions.*
 
 trait ZIODispatcher[+Env]:
   def dispatch(action: ZIO[Env, Nothing, Unit]): Unit
+  def dispatchStream[A](action: ZIO[Env, Nothing, A]): EventStream[A]
 
 object ZIODispatcher:
   def fromRuntime[Env](using runtime: Runtime[Env]): ZIODispatcher[Env] =
@@ -13,3 +16,8 @@ object ZIODispatcher:
           // TODO: do I need to cancel this on evenstream stop?
           val _ = runtime.unsafe.runToFuture(action)
         )
+
+      override def dispatchStream[A](
+          action: ZIO[Env, Nothing, A]
+      ): EventStream[A] =
+        action.toEventStream
