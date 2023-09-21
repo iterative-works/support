6c068d5af4699232d53b93e44e6cfe79369412b9
Michal Příhoda <michal@prihoda.net>
2023-09-21 10:45:47 +0200

    AkkaActorSystem self-join

--- akka-persistence/src/main/scala/iw/akka/AkkaActorSystem.scala
+++ akka-persistence/src/main/scala/iw/akka/AkkaActorSystem.scala
@@ -13,16 +13,22 @@ case class AkkaActorSystem(system: ActorSystem[?]):
     cluster.manager ! Join(cluster.selfMember.address)
   }
 
+  val terminate: UIO[Unit] = ZIO.attempt(system.terminate()).orDie
+
   given ActorSystem[?] = system
 
 object AkkaActorSystem:
-  def empty(name: String): ZLayer[Scope, Throwable, AkkaActorSystem] =
-    ZLayer(
+  private def makeEmpty(name: String): ZIO[Scope, Throwable, AkkaActorSystem] =
+    ZIO.acquireRelease(
       ZIO
-        .acquireRelease(
-          ZIO.attempt(ActorSystem(Behaviors.empty[NotUsed], name))
-        )(system => ZIO.attempt(system.terminate()).orDie)
-        .map(
-          AkkaActorSystem(_)
-        )
-    )
+        .attempt(ActorSystem(Behaviors.empty[NotUsed], name))
+        .map(AkkaActorSystem(_))
+    )(_.terminate)
+
+  def empty(name: String): ZLayer[Scope, Throwable, AkkaActorSystem] =
+    ZLayer(makeEmpty(name))
+
+  def emptySingleNodeCluster(
+      name: String
+  ): ZLayer[Scope, Throwable, AkkaActorSystem] =
+    ZLayer(makeEmpty(name).tap(_.joinSelf))
