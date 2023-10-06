c7f6244752e077ef6ffc1c4503cd6199b2a1019f
Michal Příhoda <michal@prihoda.net>
2023-10-06 15:56:42 +0200

    Update the tools versions

--- flake.nix
+++ flake.nix
@@ -1,32 +1,28 @@
 {
-  description = "IW support";
+  description = "IW project";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            (final: prev: rec {
-              jre = prev.adoptopenjdk-hotspot-bin-11;
-              jdk = jre;
-            })
+            (self: super:
+              let jvm = super.jdk17_headless;
+              in {
+                jre = jvm;
+                jdk = jvm;
+              })
           ];
         };
-      in
-      {
-        devShell = with pkgs;
+      in { devShell = with pkgs;
           mkShell {
-            buildInputs = [ jre ammonite coursier bloop sbt scalafmt nodejs-16_x ];
+            buildInputs = [ jre ammonite coursier bloop sbt scalafmt ];
           };
       });
 }
