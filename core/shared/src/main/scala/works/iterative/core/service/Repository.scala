3870cc8be098d39455afeecd44319819fe40c486
Michal Příhoda <michal@prihoda.net>
2023-11-26 00:44:57 +0100

    Formatting.

--- core/shared/src/main/scala/works/iterative/core/service/Repository.scala
+++ core/shared/src/main/scala/works/iterative/core/service/Repository.scala
@@ -4,19 +4,19 @@ import zio.*
 import zio.stream.*
 
 trait GenericLoadService[Eff[+_], -Key, +Value]:
-  type Op[A] = Eff[A]
-  def load(id: Key): Op[Option[Value]]
+    type Op[A] = Eff[A]
+    def load(id: Key): Op[Option[Value]]
 
 trait GenericUpdateNotifyService[Str[+_], Key]:
-  def updates: Str[Key]
+    def updates: Str[Key]
 
 trait GenericLoadAllService[Eff[+_], Coll[+_], -Key, +Value]:
-  type Op[A] = Eff[A]
-  def loadAll(ids: Seq[Key]): Op[Coll[Value]]
+    type Op[A] = Eff[A]
+    def loadAll(ids: Seq[Key]): Op[Coll[Value]]
 
 trait GenericFindService[Eff[+_], Coll[+_], -Key, +Value, -FilterArg]:
-  type Op[A] = Eff[A]
-  def find(filter: FilterArg): Op[Coll[Value]]
+    type Op[A] = Eff[A]
+    def find(filter: FilterArg): Op[Coll[Value]]
 
 trait GenericReadRepository[Eff[+_], Coll[+_], -Key, +Value, -FilterArg]
     extends GenericLoadService[Eff, Key, Value]
@@ -24,8 +24,8 @@ trait GenericReadRepository[Eff[+_], Coll[+_], -Key, +Value, -FilterArg]
     with GenericFindService[Eff, Coll, Key, Value, FilterArg]
 
 trait GenericWriteRepository[Eff[_], -Key, -Value]:
-  type Op[A] = Eff[A]
-  def save(key: Key, value: Value): Op[Unit]
+    type Op[A] = Eff[A]
+    def save(key: Key, value: Value): Op[Unit]
 
 trait GenericRepository[Eff[+_], -Key, Value]
     extends GenericReadRepository[Eff, List, Key, Value, Unit]
@@ -33,13 +33,14 @@ trait GenericRepository[Eff[+_], -Key, Value]
 
 type LoadRepository[-Key, +Value] = GenericLoadService[UIO, Key, Value]
 type LoadAllRepository[-Key, +Value] =
-  GenericLoadAllService[UIO, List, Key, Value]
+    GenericLoadAllService[UIO, List, Key, Value]
 
 trait ReadRepository[-Key, +Value, -FilterArg]
     extends GenericReadRepository[UIO, List, Key, Value, FilterArg]:
-  override def loadAll(ids: Seq[Key]): UIO[List[Value]] =
-    // Inefficient implementation, meant to be overridden
-    ZIO.foreach(ids)(load).map(_.flatten.toList)
+    override def loadAll(ids: Seq[Key]): UIO[List[Value]] =
+        // Inefficient implementation, meant to be overridden
+        ZIO.foreach(ids)(load).map(_.flatten.toList)
+end ReadRepository
 
 trait UpdateNotifyRepository[Key]
     extends GenericUpdateNotifyService[UStream, Key]
--- ui/js/src/main/scala/works/iterative/ui/components/FileComponents.scala
+++ ui/js/src/main/scala/works/iterative/ui/components/FileComponents.scala
@@ -4,13 +4,14 @@ import com.raquo.laminar.api.L.*
 import works.iterative.core.FileRef
 
 trait FileComponents:
-  // I hate using ComponentContext all around, we need to drop it
-  def renderFileRefs(
-      ref: Seq[FileRef],
-      metadata: Option[(FileRef, Int) => Map[String, String]] = None,
-      rolledUp: Boolean = false
-  )(using
-      ComponentContext[?]
-  ): HtmlElement
+    // I hate using ComponentContext all around, we need to drop it
+    def renderFileRefs(
+        ref: Seq[FileRef],
+        metadata: Option[(FileRef, Int) => Map[String, String]] = None,
+        rolledUp: Boolean = false
+    )(using
+        ComponentContext[?]
+    ): HtmlElement
 
-  def renderFileLink(ref: FileRef)(using ComponentContext[?]): HtmlElement
+    def renderFileLink(ref: FileRef)(using ComponentContext[?]): HtmlElement
+end FileComponents
