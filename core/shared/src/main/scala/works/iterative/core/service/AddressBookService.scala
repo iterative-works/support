9918424ec340ff132cf4ccf92e9e409786ffb15a
Michal Příhoda <michal@prihoda.net>
2023-11-26 21:33:40 +0100

    Formatting.

--- core/shared/src/main/scala/works/iterative/core/service/AddressBookService.scala
+++ core/shared/src/main/scala/works/iterative/core/service/AddressBookService.scala
@@ -5,10 +5,11 @@ import works.iterative.core.auth.PermissionTarget
 import works.iterative.core.Email
 
 trait AddressBookService:
-  def emailOfTarget(target: PermissionTarget): UIO[List[Email]]
+    def emailOfTarget(target: PermissionTarget): UIO[List[Email]]
 
 object AddressBookService:
-  def emailOfTarget(
-      target: PermissionTarget
-  ): URIO[AddressBookService, List[Email]] =
-    ZIO.serviceWithZIO(_.emailOfTarget(target))
+    def emailOfTarget(
+        target: PermissionTarget
+    ): URIO[AddressBookService, List[Email]] =
+        ZIO.serviceWithZIO(_.emailOfTarget(target))
+end AddressBookService
