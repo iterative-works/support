feeacc030690b32e5329c0795e3be33f1df7491f
Michal Příhoda <michal@prihoda.net>
2023-11-30 01:08:15 +0100

    Removed unused imports.

--- server/http/src/main/scala/works/iterative/server/http/impl/pac4j/Pac4jConfigFactory.scala
+++ server/http/src/main/scala/works/iterative/server/http/impl/pac4j/Pac4jConfigFactory.scala
@@ -4,12 +4,7 @@ package impl.pac4j
 import org.pac4j.core.config.*
 import org.pac4j.oidc.client.OidcClient
 import org.pac4j.oidc.config.OidcConfiguration
-import org.pac4j.core.authorization.generator.AuthorizationGenerator
 import org.pac4j.http4s.{DefaultHttpActionAdapter, Http4sCacheSessionStore}
-import org.pac4j.core.context.WebContext
-import org.pac4j.core.context.session.SessionStore
-import org.pac4j.core.profile.UserProfile
-import java.util.Optional
 import cats.effect.Sync
 import org.pac4j.core.client.Clients
 import org.pac4j.core.authorization.generator.FromAttributesAuthorizationGenerator
