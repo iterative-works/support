26886891e653a1218f1d1543149c291deff37454
Michal Příhoda <michal@prihoda.net>
2023-11-30 10:21:21 +0100

    Better error reporting.

--- ui/js/src/main/scala/works/iterative/ui/components/ReloadableComponent.scala
+++ ui/js/src/main/scala/works/iterative/ui/components/ReloadableComponent.scala
@@ -18,107 +18,129 @@ case class ReloadableComponent[A, I](
     updates: Option[UStream[ReloadableComponent.Reload[A]]] = None,
     loadSchedule: Schedule[Any, Any, ?] = Schedule.stop
 )(using runtime: Runtime[Any]):
-  import ReloadableComponent.Reload
+    import ReloadableComponent.Reload
 
-  private val computable: Var[Computable[A]] = Var(Computable.Uninitialized)
-  private val memo: Var[Option[I]] = Var(init)
+    private val computable: Var[Computable[A]] = Var(Computable.Uninitialized)
+    private val memo: Var[Option[I]] = Var(init)
 
-  val state: Signal[Computable[A]] = computable.signal
+    val state: Signal[Computable[A]] = computable.signal
 
-  def now(): Option[A] = computable.now().toOption
+    def now(): Option[A] = computable.now().toOption
 
-  val update: Observer[I] = Observer { input =>
-    memo.update(_ => Some(input))
-    load(input)
-  }
-
-  val reload: Observer[Reload[A]] = Observer {
-    case Reload.Once => memo.now().foreach(load)
-    case Reload.UntilChanged(original) =>
-      memo.now().foreach(reloadUntilChanged(_, original))
-  }
-
-  private def eventStreamFromZioStream[A](
-      eff: UStream[A]
-  ): EventStream[A] =
-    var runningFiber: Option[Fiber.Runtime[Nothing, Unit]] = None
-    EventStream
-      .fromCustomSource(
-        shouldStart = _ => true,
-        start = (fireValue, _, _, _) => {
-          runningFiber = Some(Unsafe.unsafely {
-            runtime.unsafe.fork(
-              eff.runForeach(v => ZIO.succeed(fireValue(v)))
-            )
-          })
-        },
-        stop = _ => {
-          runningFiber.foreach { f =>
-            Unsafe.unsafely {
-              runtime.unsafe.fork(f.interrupt)
-            }
-          }
-        }
-      )
-
-  private def updateFromZioStream(
-      upd: UStream[Reload[A]]
-  ): HtmlMod =
-    onMountBind { _ =>
-      eventStreamFromZioStream(upd) --> reload
+    val update: Observer[I] = Observer { input =>
+        memo.update(_ => Some(input))
+        load(input)
     }
 
-  private def updateStream: HtmlMod = updates match
+    val reload: Observer[Reload[A]] = Observer {
+        case Reload.Once => memo.now().foreach(load)
+        case Reload.UntilChanged(original) =>
+            memo.now().foreach(reloadUntilChanged(_, original))
+    }
+
+    private def eventStreamFromZioStream[A](
+        eff: UStream[A]
+    ): EventStream[A] =
+        var runningFiber: Option[Fiber.Runtime[Nothing, Unit]] = None
+        EventStream
+            .fromCustomSource(
+                shouldStart = _ => true,
+                start = (fireValue, _, _, _) =>
+                    runningFiber = Some(Unsafe.unsafely {
+                        runtime.unsafe.fork(
+                            eff.runForeach(v => ZIO.succeed(fireValue(v)))
+                        )
+                    }),
+                stop = _ =>
+                    runningFiber.foreach { f =>
+                        Unsafe.unsafely {
+                            runtime.unsafe.fork(f.interrupt)
+                        }
+                    }
+            )
+    end eventStreamFromZioStream
+
+    private def updateFromZioStream(
+        upd: UStream[Reload[A]]
+    ): HtmlMod =
+        onMountBind { _ =>
+            eventStreamFromZioStream(upd) --> reload
+        }
+
+    private def updateStream: HtmlMod = updates match
     case None      => emptyMod
     case Some(upd) => updateFromZioStream(upd)
 
-  def initMod: HtmlMod = nodeSeq(
-    EventStream.fromValue(ReloadableComponent.Reload.Once) --> reload,
-    updateStream
-  )
-
-  def load(input: I): Unit = doLoad(
-    input,
-    fetch(_).retry(loadSchedule)
-  )
-
-  def reloadUntilChanged(input: I, original: A): Unit =
-    doLoad(
-      input,
-      fetch(_)
-        .repeat(loadSchedule && Schedule.recurWhile(_ == original))
-        .map(_._2)
+    def initMod: HtmlMod = nodeSeq(
+        EventStream.fromValue(ReloadableComponent.Reload.Once) --> reload,
+        updateStream
     )
 
-  private def doLoad(
-      input: I,
-      q: I => IO[UserMessage, A]
-  ): Unit =
-    computable.update(_.started)
-    // TODO: do we need to manage the result of the run?
-    val _ = Unsafe.unsafely {
-      runtime.unsafe.runOrFork(
-        q(input).fold(
-          msg => computable.update(_.fail(msg)),
-          result => computable.update(_.update(result))
+    def load(input: I): Unit = doLoad(
+        input,
+        fetch(_).retry(loadSchedule)
+    )
+
+    def reloadUntilChanged(input: I, original: A): Unit =
+        doLoad(
+            input,
+            fetch(_)
+                .repeat(loadSchedule && Schedule.recurWhile(_ == original))
+                .map(_._2)
         )
-      )
-    }
+
+    private def doLoad(
+        input: I,
+        q: I => IO[UserMessage, A]
+    ): Unit =
+        computable.update:
+            _.started
+
+        // TODO: do we need to manage the result of the run?
+        val _ = Unsafe.unsafely {
+            runtime.unsafe.runOrFork(
+                q(input).foldCauseZIO(
+                    c =>
+                        c.failureOrCause.fold(
+                            msg =>
+                                ZIO.succeed:
+                                    computable.update:
+                                        _.fail(msg)
+                            ,
+                            t =>
+                                for
+                                    id <- Random.nextUUID.map(_.toString())
+                                    _ <- ZIO.when(c.isDie)(ZIO.logErrorCause(c))
+                                    _ <- ZIO.succeed:
+                                        computable.update:
+                                            _.fail(UserMessage("error.unexpected", id))
+                                yield ()
+                        ),
+                    result =>
+                        ZIO.succeed:
+                            computable.update:
+                                _.update(result)
+                )
+            )
+        }
+    end doLoad
+end ReloadableComponent
 
 object ReloadableComponent:
-  enum Reload[+A]:
-    case Once extends Reload[Nothing]
-    case UntilChanged(a: A) extends Reload[A]
+    enum Reload[+A]:
+        case Once extends Reload[Nothing]
+        case UntilChanged(a: A) extends Reload[A]
 
-  def apply[A](fetch: IO[UserMessage, A])(using
-      runtime: Runtime[Any]
-  ): ReloadableComponent[A, Unit] =
-    ReloadableComponent(_ => fetch, Some(()))
+    def apply[A](fetch: IO[UserMessage, A])(using
+        runtime: Runtime[Any]
+    ): ReloadableComponent[A, Unit] =
+        ReloadableComponent(_ => fetch, Some(()))
 
-  def apply[A, I](
-      endpoint: PublicEndpoint[I, Unit, A, Any]
-  ): URIO[ClientEndpointFactory, ReloadableComponent[A, I]] =
-    for
-      given Runtime[Any] <- ZIO.runtime[Any]
-      factory <- ZIO.service[ClientEndpointFactory]
-    yield new ReloadableComponent(factory.make(endpoint))
+    def apply[A, I](
+        endpoint: PublicEndpoint[I, Unit, A, Any]
+    ): URIO[ClientEndpointFactory, ReloadableComponent[A, I]] =
+        for
+            given Runtime[Any] <- ZIO.runtime[Any]
+            factory <- ZIO.service[ClientEndpointFactory]
+        yield new ReloadableComponent(factory.make(endpoint))
+end ReloadableComponent
