6756c441909125c2ca49c68a132511d308751845
Michal Příhoda <michal@prihoda.net>
2023-12-11 08:07:07 +0100

    Add Redoc documentation for documentSeal API

--- server/http/src/main/scala/works/iterative/server/http/HttpServer.scala
+++ server/http/src/main/scala/works/iterative/server/http/HttpServer.scala
@@ -1,15 +1,21 @@
 package works.iterative.server.http
 
 import zio.*
+import zio.interop.catz.*
 import works.iterative.core.auth.service.AuthenticationService
+import org.http4s.HttpRoutes
 
 trait HttpServer:
-  def serve[Env <: AuthenticationService](
-      app: HttpApplication[Env]
-  ): URIO[Env, Nothing]
+    def serve[Env <: AuthenticationService](
+        app: HttpApplication[Env],
+        extraRoutes: HttpRoutes[RIO[Env, *]] = HttpRoutes.empty
+    ): URIO[Env, Nothing]
+end HttpServer
 
 object HttpServer:
-  def serve[Env <: AuthenticationService](
-      app: HttpApplication[Env]
-  ): URIO[Env & HttpServer, Nothing] =
-    ZIO.serviceWithZIO[HttpServer](_.serve(app))
+    def serve[Env <: AuthenticationService](
+        app: HttpApplication[Env],
+        extraRoutes: HttpRoutes[RIO[Env, *]] = HttpRoutes.empty
+    ): URIO[Env & HttpServer, Nothing] =
+        ZIO.serviceWithZIO[HttpServer](_.serve(app, extraRoutes))
+end HttpServer
--- server/http/src/main/scala/works/iterative/server/http/impl/blaze/BlazeHttpServer.scala
+++ server/http/src/main/scala/works/iterative/server/http/impl/blaze/BlazeHttpServer.scala
@@ -28,100 +28,104 @@ class BlazeHttpServer(
     baseUri: BaseUri,
     updateProfile: (OidcProfile, BasicProfile) => BasicProfile
 ) extends HttpServer:
-  override def serve[Env <: AuthenticationService](
-      app: HttpApplication[Env]
-  ): URIO[Env, Nothing] =
-    type AppTask[A] = RIO[Env, A]
-    type SecuredTask[A] = RIO[Env & CurrentUser, A]
+    override def serve[Env <: AuthenticationService](
+        app: HttpApplication[Env],
+        extraRoutes: HttpRoutes[RIO[Env, *]] = HttpRoutes.empty
+    ): URIO[Env, Nothing] =
+        type AppTask[A] = RIO[Env, A]
+        type SecuredTask[A] = RIO[Env & CurrentUser, A]
 
-    ZIO.runtime[Env].flatMap { runtime =>
-      val contextBuilder =
-        (req: Request[AppTask], conf: org.pac4j.core.config.Config) =>
-          new Http4sWebContext[AppTask](
-            req,
-            conf.getSessionStore,
-            t =>
-              Unsafe.unsafely(
-                runtime.unsafe.run(t).getOrThrowFiberFailure()
-              )
-          )
+        ZIO.runtime[Env].flatMap { runtime =>
+            val contextBuilder =
+                (req: Request[AppTask], conf: org.pac4j.core.config.Config) =>
+                    new Http4sWebContext[AppTask](
+                        req,
+                        conf.getSessionStore,
+                        t =>
+                            Unsafe.unsafely(
+                                runtime.unsafe.run(t).getOrThrowFiberFailure()
+                            )
+                    )
 
-      val pac4jSecurity =
-        Pac4jHttpSecurity[AppTask](
-          baseUri,
-          pac4jConfig,
-          contextBuilder,
-          updateProfile
-        )
+            val pac4jSecurity =
+                Pac4jHttpSecurity[AppTask](
+                    baseUri,
+                    pac4jConfig,
+                    contextBuilder,
+                    updateProfile
+                )
 
-      // TODO: remove the SecuredTask and provide just the authentication when the move to AuthenticationService is done.
-      def provideCurrentUser(
-          routes: HttpRoutes[SecuredTask]
-      ): HttpRoutes[AppTask] =
-        def secureRoutes: AuthedRoutes[AuthedUserInfo, AppTask] =
-          Kleisli { ctx =>
-            val authedUserInfo = ctx.context
-            val userEnv = ZEnvironment(CurrentUser(authedUserInfo.profile))
+            // TODO: remove the SecuredTask and provide just the authentication when the move to AuthenticationService is done.
+            def provideCurrentUser(
+                routes: HttpRoutes[SecuredTask]
+            ): HttpRoutes[AppTask] =
+                def secureRoutes: AuthedRoutes[AuthedUserInfo, AppTask] =
+                    Kleisli { ctx =>
+                        val authedUserInfo = ctx.context
+                        val userEnv = ZEnvironment(CurrentUser(authedUserInfo.profile))
 
-            // Just add CurrentUser to the env, the effect does not need it anyway
-            val widenCurrentUser: AppTask ~> SecuredTask =
-              new FunctionK[AppTask, SecuredTask]:
-                override def apply[A](fa: AppTask[A]): SecuredTask[A] = fa
+                        // Just add CurrentUser to the env, the effect does not need it anyway
+                        val widenCurrentUser: AppTask ~> SecuredTask =
+                            new FunctionK[AppTask, SecuredTask]:
+                                override def apply[A](fa: AppTask[A]): SecuredTask[A] = fa
 
-            // Provide
-            val eliminateCurrentUser: SecuredTask ~> AppTask =
-              new FunctionK[SecuredTask, AppTask]:
-                override def apply[A](fa: SecuredTask[A]): AppTask[A] =
-                  AuthenticationService.loggedIn(authedUserInfo) *> fa
-                    .provideSomeEnvironment[Env](env => env ++ userEnv)
+                        // Provide
+                        val eliminateCurrentUser: SecuredTask ~> AppTask =
+                            new FunctionK[SecuredTask, AppTask]:
+                                override def apply[A](fa: SecuredTask[A]): AppTask[A] =
+                                    AuthenticationService.loggedIn(authedUserInfo) *> fa
+                                        .provideSomeEnvironment[Env](env => env ++ userEnv)
 
-            routes
-              .run(ctx.req.mapK(widenCurrentUser))
-              .map(_.mapK(eliminateCurrentUser))
-              .mapK(eliminateCurrentUser)
-          }
+                        routes
+                            .run(ctx.req.mapK(widenCurrentUser))
+                            .map(_.mapK(eliminateCurrentUser))
+                            .mapK(eliminateCurrentUser)
+                    }
 
-        pac4jSecurity.secure(secureRoutes)
+                pac4jSecurity.secure(secureRoutes)
+            end provideCurrentUser
 
-      val publicInterpreter = new Http4sCustomTapir[Env] {}
-      val securedInterpreter = new Http4sCustomTapir[Env & CurrentUser] {}
+            val publicInterpreter = new Http4sCustomTapir[Env] {}
+            val securedInterpreter = new Http4sCustomTapir[Env & CurrentUser] {}
 
-      val publicRoutes: HttpRoutes[AppTask] =
-        publicInterpreter.from(app.publicEndpoints).toRoutes
-      val securedRoutes: HttpRoutes[SecuredTask] =
-        securedInterpreter.from(app.secureEndpoints).toRoutes
+            val publicRoutes: HttpRoutes[AppTask] =
+                publicInterpreter.from(app.publicEndpoints).toRoutes
+            val securedRoutes: HttpRoutes[SecuredTask] =
+                securedInterpreter.from(app.secureEndpoints).toRoutes
 
-      val wsRoutes: WebSocketBuilder2[AppTask] => HttpRoutes[AppTask] =
-        publicInterpreter.fromWebSocket(app.wsEndpoints).toRoutes
+            val wsRoutes: WebSocketBuilder2[AppTask] => HttpRoutes[AppTask] =
+                publicInterpreter.fromWebSocket(app.wsEndpoints).toRoutes
 
-      val eliminated: HttpRoutes[AppTask] = provideCurrentUser(securedRoutes)
+            val eliminated: HttpRoutes[AppTask] = provideCurrentUser(securedRoutes)
 
-      def withBaseUri(routes: HttpRoutes[AppTask]): HttpRoutes[AppTask] =
-        baseUri.value match
-          case Some(u) => Router(u.toString -> routes)
-          case _       => routes
+            def withBaseUri(routes: HttpRoutes[AppTask]): HttpRoutes[AppTask] =
+                baseUri.value match
+                case Some(u) => Router(u.toString -> routes)
+                case _       => routes
 
-      BlazeServerBuilder[AppTask]
-        .bindHttp(config.port, config.host)
-        .withHttpWebSocketApp(wsb =>
-          (pac4jSecurity.route <+> withBaseUri(
-            wsRoutes(wsb) <+> publicRoutes <+> eliminated
-          )).orNotFound
-        )
-        .serve
-        .compile
-        .drain
-        .orDie *> ZIO.never
-    }
+            BlazeServerBuilder[AppTask]
+                .bindHttp(config.port, config.host)
+                .withHttpWebSocketApp(wsb =>
+                    (pac4jSecurity.route <+> withBaseUri(extraRoutes <+>
+                        wsRoutes(wsb) <+> publicRoutes <+> eliminated)).orNotFound
+                )
+                .serve
+                .compile
+                .drain
+                .orDie *> ZIO.never
+        }
+    end serve
+end BlazeHttpServer
 
 object BlazeHttpServer:
-  def layer(
-      updateProfile: (OidcProfile, BasicProfile) => BasicProfile = (_, u) => u
-  ): RLayer[BlazeServerConfig & Pac4jSecurityConfig & BaseUri, HttpServer] =
-    ZLayer {
-      for
-        config <- ZIO.service[BlazeServerConfig]
-        pac4jConfig <- ZIO.service[Pac4jSecurityConfig]
-        baseUri <- ZIO.service[BaseUri]
-      yield BlazeHttpServer(config, pac4jConfig, baseUri, updateProfile)
-    }
+    def layer(
+        updateProfile: (OidcProfile, BasicProfile) => BasicProfile = (_, u) => u
+    ): RLayer[BlazeServerConfig & Pac4jSecurityConfig & BaseUri, HttpServer] =
+        ZLayer {
+            for
+                config <- ZIO.service[BlazeServerConfig]
+                pac4jConfig <- ZIO.service[Pac4jSecurityConfig]
+                baseUri <- ZIO.service[BaseUri]
+            yield BlazeHttpServer(config, pac4jConfig, baseUri, updateProfile)
+        }
+end BlazeHttpServer
