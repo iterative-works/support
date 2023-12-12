057d413d1c56a3ebda78f1aad8b2678885a6f3e0
Michal Příhoda <michal@prihoda.net>
2023-12-12 09:38:31 +0100

    Refactor BaseUri and LiveClientEndpointFactory

--- tapir/shared/src/main/scala/works/iterative/tapir/LiveClientEndpointFactory.scala
+++ tapir/shared/src/main/scala/works/iterative/tapir/LiveClientEndpointFactory.scala
@@ -12,51 +12,54 @@ class LiveClientEndpointFactory(using
 ) extends ClientEndpointFactory
     with CustomTapir:
 
-  def makeRequest[S, I, E, O](
-      endpoint: Endpoint[S, I, E, O, ZioStreams & WebSockets]
-  )(using ext: BaseUriExtractor[O]) = toSecureRequest(
-    endpoint,
-    ext.extractBaseUri
-  )
+    def makeRequest[S, I, E, O](
+        endpoint: Endpoint[S, I, E, O, ZioStreams & WebSockets]
+    )(using ext: BaseUriExtractor[O]) = toSecureRequest(
+        endpoint,
+        ext.extractBaseUri
+    )
 
-  override def make[S, I, E, O](
-      endpoint: Endpoint[S, I, E, O, ZioStreams & WebSockets]
-  )(using
-      ext: BaseUriExtractor[O],
-      em: ClientErrorConstructor[E],
-      m: ClientResultConstructor[S, I, em.Error, O]
-  ): m.Result = m.makeResult((securityInput: S) =>
-    (input: I) =>
-      val req = makeRequest(endpoint)
-      val fetch = req(securityInput)(input)
-        .header("is_ajax_request", "true")
-        .followRedirects(false)
-        .send(backend)
+    override def make[S, I, E, O](
+        endpoint: Endpoint[S, I, E, O, ZioStreams & WebSockets]
+    )(using
+        ext: BaseUriExtractor[O],
+        em: ClientErrorConstructor[E],
+        m: ClientResultConstructor[S, I, em.Error, O]
+    ): m.Result = m.makeResult((securityInput: S) =>
+        (input: I) =>
+            val req = makeRequest(endpoint)
+            val fetch = req(securityInput)(input)
+                .header("X-Requested-With", "XMLHttpRequest")
+                .followRedirects(false)
+                .send(backend)
 
-      val result = for
-        resp <- fetch.orDie
-        body <- resp.body match
-          case DecodeResult.Value(v) => ZIO.succeed(v)
-          case err: DecodeResult.Failure =>
-            ZIO.die(
-              new RuntimeException(
-                s"Unexpected response status: ${resp.code} ${resp.statusText} - ${err}"
-              )
-            )
-        v <- ZIO.fromEither(body)
-      yield v
+            val result =
+                for
+                    resp <- fetch.orDie
+                    body <- resp.body match
+                    case DecodeResult.Value(v) => ZIO.succeed(v)
+                    case err: DecodeResult.Failure =>
+                        ZIO.die(
+                            new RuntimeException(
+                                s"Unexpected response status: ${resp.code} ${resp.statusText} - ${err}"
+                            )
+                        )
+                    v <- ZIO.fromEither(body)
+                yield v
 
-      em.mapErrorCause(result)
-  )
+            em.mapErrorCause(result)
+    )
+end LiveClientEndpointFactory
 
 object LiveClientEndpointFactory:
-  val layer: URLayer[BaseUri & CustomTapir.Backend, ClientEndpointFactory] =
-    ZLayer {
-      for
-        given BaseUri <- ZIO.service[BaseUri]
-        given CustomTapir.Backend <- ZIO.service[CustomTapir.Backend]
-      yield LiveClientEndpointFactory()
-    }
+    val layer: URLayer[BaseUri & CustomTapir.Backend, ClientEndpointFactory] =
+        ZLayer {
+            for
+                given BaseUri <- ZIO.service[BaseUri]
+                given CustomTapir.Backend <- ZIO.service[CustomTapir.Backend]
+            yield LiveClientEndpointFactory()
+        }
 
-  val default: ZLayer[BaseUri, Throwable, ClientEndpointFactory] =
-    CustomTapir.clientLayer >>> layer
+    val default: ZLayer[BaseUri, Throwable, ClientEndpointFactory] =
+        CustomTapir.clientLayer >>> layer
+end LiveClientEndpointFactory
