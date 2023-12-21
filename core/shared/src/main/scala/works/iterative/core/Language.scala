f56032029dbeeceecab70d8db384c57e7a75f14b
Michal Příhoda <michal@prihoda.net>
2023-12-21 09:44:27 +0100

    Formatting.

--- core/shared/src/main/scala/works/iterative/core/Language.scala
+++ core/shared/src/main/scala/works/iterative/core/Language.scala
@@ -3,15 +3,17 @@ package works.iterative.core
 opaque type Language = String
 
 object Language:
-  val CS: Language = "cs"
-  val EN: Language = "en"
-  val DE: Language = "de"
+    val CS: Language = "cs"
+    val EN: Language = "en"
+    val DE: Language = "de"
 
-  extension (l: Language) def value: String = l
+    extension (l: Language) def value: String = l
+end Language
 
 case class LanguagePreference(inOrder: List[Language])
 
 object LanguagePreference:
-  given defaultLanguagePreference: LanguagePreference = LanguagePreference(
-    List(Language.CS, Language.EN)
-  )
+    given defaultLanguagePreference: LanguagePreference = LanguagePreference(
+        List(Language.CS, Language.EN)
+    )
+end LanguagePreference
