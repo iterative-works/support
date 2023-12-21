e0bfeabdb417c129cf4edbe0f52caec88142abc3
Michal Příhoda <michal@prihoda.net>
2023-12-21 09:45:30 +0100

    Formatting.

--- codecs/src/main/scala/works/iterative/core/codecs/Codecs.scala
+++ codecs/src/main/scala/works/iterative/core/codecs/Codecs.scala
@@ -9,9 +9,9 @@ import works.iterative.event.EventRecord
 trait Codecs extends JsonCodecs with TapirCodecs
 
 trait JsonCodecs extends works.iterative.tapir.codecs.JsonCodecs:
-  given JsonCodec[EventRecord] = DeriveJsonCodec.gen[EventRecord]
+    given JsonCodec[EventRecord] = DeriveJsonCodec.gen[EventRecord]
 
 trait TapirCodecs extends works.iterative.tapir.codecs.TapirCodecs:
-  given Schema[EventRecord] = Schema.derived[EventRecord]
+    given Schema[EventRecord] = Schema.derived[EventRecord]
 
 object Codecs extends Codecs
