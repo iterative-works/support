0564ffd1a9b9b4bbf83533ba0f8b2ec65fd0f4de
Michal Příhoda <michal@prihoda.net>
2024-01-07 22:33:05 +0100

    Formatting

--- email/src/main/scala/works/iterative/service/email/EmailNotificationService.scala
+++ email/src/main/scala/works/iterative/service/email/EmailNotificationService.scala
@@ -18,76 +18,82 @@ case class Attachment(
 )
 
 object Attachment:
-  def apply(
-      filename: String,
-      mimeType: String,
-      content: Array[Byte]
-  ): Attachment =
-    Attachment(filename, mimeType, content, filename)
+    def apply(
+        filename: String,
+        mimeType: String,
+        content: Array[Byte]
+    ): Attachment =
+        Attachment(filename, mimeType, content, filename)
+end Attachment
 
 trait EmailNotificationService:
-  type Op[A] = ZIO[Any, EmailNotificationService.Error, A]
+    type Op[A] = ZIO[Any, EmailNotificationService.Error, A]
 
-  def sendEmail(
-      subject: String,
-      content: String,
-      attachments: Attachment*
-  )(to: Email*): Op[Unit] =
-    sendEmail(EmailContent(subject, content, attachments*))(to*)
+    def sendEmail(
+        subject: String,
+        content: String,
+        attachments: Attachment*
+    )(to: Email*): Op[Unit] =
+        sendEmail(EmailContent(subject, content, attachments*))(to*)
 
-  def sendEmail(content: EmailContent)(to: Email*): Op[Unit]
+    def sendEmail(content: EmailContent)(to: Email*): Op[Unit]
+end EmailNotificationService
 
 object EmailNotificationService:
-  // Accessors
-  def sendEmail(
-      subject: String,
-      content: String,
-      attachments: Attachment*
-  )(to: Email*): ZIO[EmailNotificationService, Error, Unit] =
-    ZIO.serviceWithZIO[EmailNotificationService](
-      _.sendEmail(subject, content, attachments*)(to*)
-    )
+    // Accessors
+    def sendEmail(
+        subject: String,
+        content: String,
+        attachments: Attachment*
+    )(to: Email*): ZIO[EmailNotificationService, Error, Unit] =
+        ZIO.serviceWithZIO[EmailNotificationService](
+            _.sendEmail(subject, content, attachments*)(to*)
+        )
 
-  def sendEmail(content: EmailContent)(to: Email*): ZIO[
-    EmailNotificationService,
-    Error,
-    Unit
-  ] =
-    ZIO.serviceWithZIO[EmailNotificationService](_.sendEmail(content)(to*))
+    def sendEmail(content: EmailContent)(to: Email*): ZIO[
+        EmailNotificationService,
+        Error,
+        Unit
+    ] =
+        ZIO.serviceWithZIO[EmailNotificationService](_.sendEmail(content)(to*))
 
-  // Errors
-  sealed trait Error:
-    def message: String
+    // Errors
+    sealed trait Error:
+        def message: String
 
-  object Error:
-    case class ConfigurationError(message: String, ex: Option[Throwable])
-        extends Error
+    object Error:
+        case class ConfigurationError(message: String, ex: Option[Throwable])
+            extends Error
 
-    object ConfigurationError:
-      def apply(message: String): ConfigurationError =
-        ConfigurationError(message, None)
+        object ConfigurationError:
+            def apply(message: String): ConfigurationError =
+                ConfigurationError(message, None)
 
-      def apply(message: String, ex: Throwable): ConfigurationError =
-        ConfigurationError(message, Some(ex))
+            def apply(message: String, ex: Throwable): ConfigurationError =
+                ConfigurationError(message, Some(ex))
 
-      def apply(ex: Throwable): ConfigurationError =
-        ConfigurationError(ex.getMessage(), Some(ex))
+            def apply(ex: Throwable): ConfigurationError =
+                ConfigurationError(ex.getMessage(), Some(ex))
+        end ConfigurationError
 
-    case class TransportFailed(message: String, ex: Option[Throwable])
-        extends Error
+        case class TransportFailed(message: String, ex: Option[Throwable])
+            extends Error
 
-    object TransportFailed:
-      def apply(message: String): TransportFailed =
-        TransportFailed(message, None)
+        object TransportFailed:
+            def apply(message: String): TransportFailed =
+                TransportFailed(message, None)
 
-      def apply(message: String, ex: Throwable): TransportFailed =
-        TransportFailed(message, Some(ex))
+            def apply(message: String, ex: Throwable): TransportFailed =
+                TransportFailed(message, Some(ex))
 
-      def apply(ex: Throwable): TransportFailed =
-        TransportFailed(ex.getMessage(), Some(ex))
+            def apply(ex: Throwable): TransportFailed =
+                TransportFailed(ex.getMessage(), Some(ex))
+        end TransportFailed
 
-    case class InvalidRequest(message: String) extends Error
+        case class InvalidRequest(message: String) extends Error
 
-    object InvalidRequest:
-      def apply(ex: Throwable): InvalidRequest =
-        InvalidRequest(ex.getMessage())
+        object InvalidRequest:
+            def apply(ex: Throwable): InvalidRequest =
+                InvalidRequest(ex.getMessage())
+    end Error
+end EmailNotificationService
