0f182680672ee3cf24b97415ab8917d793b55faf
Michal Příhoda <michal@prihoda.net>
2024-01-15 21:51:22 +0100

    Fix autocomplete service resolution
    MySQLAddressBookService has been resolved for Autocomplete dependency even though it did not contributed the Autocomplete trait to the layer (just implementing it).
    
    Moved the autocomplete to sub object.

--- autocomplete/jvm/src/main/scala/works/iterative/autocomplete/service/impl/rest/AutocompleteApi.scala
+++ autocomplete/jvm/src/main/scala/works/iterative/autocomplete/service/impl/rest/AutocompleteApi.scala
@@ -2,13 +2,20 @@ package works.iterative.autocomplete
 package service
 package impl.rest
 
+import zio.*
 import works.iterative.autocomplete.endpoints.AutocompleteEndpoints
 import works.iterative.tapir.CustomTapir.*
 
 trait AutocompleteApi(endpoints: AutocompleteEndpoints):
     val find: ZServerEndpoint[AutocompleteService, Any] =
         endpoints.find.zServerLogic((collection, q, limit, lang) =>
-            AutocompleteService.find(collection, q, limit, lang)
+            for
+                as <- ZIO.service[AutocompleteService]
+                _ <- ZIO.logError(
+                    s"AutocompleteService: $as"
+                )
+                result <- as.find(collection, q, limit, lang)
+            yield result
         )
 
     val load: ZServerEndpoint[AutocompleteService, Any] =
