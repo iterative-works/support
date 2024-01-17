4e284d7cbe063b3dcacc6ef41702c17a23894f81
Michal Příhoda <michal@prihoda.net>
2024-01-17 20:25:09 +0100

    Optional field support, plusDays in Moment

--- core/shared/src/main/scala/works/iterative/core/Moment.scala
+++ core/shared/src/main/scala/works/iterative/core/Moment.scala
@@ -6,22 +6,24 @@ import zio.*
 
 /** A moment in time, represented as an instant
   *
-  * Why not to use Instant by itself? Well, the time representation might change
-  * for some reason, or might be different in different contexts (Like ScalaJS
-  * or Native). Better to abstract it away.
+  * Why not to use Instant by itself? Well, the time representation might change for some reason, or
+  * might be different in different contexts (Like ScalaJS or Native). Better to abstract it away.
   */
 opaque type Moment = Instant
 
 object Moment:
-  def apply(value: Instant): Moment = value
+    def apply(value: Instant): Moment = value
 
-  def now: UIO[Moment] =
-    for now <- Clock.instant
-    yield Moment(now)
+    def now: UIO[Moment] =
+        for now <- Clock.instant
+        yield Moment(now)
 
-  extension (m: Moment) def toInstant: Instant = m
+    extension (m: Moment)
+        def toInstant: Instant = m
+        def plusDays(days: Int): Moment = m.plusSeconds(days * 24 * 60 * 60)
 
-  given Conversion[Instant, Moment] = Moment(_)
-  given Conversion[Moment, Instant] = _.toInstant
+    given Conversion[Instant, Moment] = Moment(_)
+    given Conversion[Moment, Instant] = _.toInstant
 
-  given (using ord: Ordering[Instant]): Ordering[Moment] = ord
+    given (using ord: Ordering[Instant]): Ordering[Moment] = ord
+end Moment
