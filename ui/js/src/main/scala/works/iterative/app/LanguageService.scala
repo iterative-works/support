683acea13f30d922f9cb415b9150770b99dc19bc
Michal Příhoda <michal@prihoda.net>
2024-01-29 15:04:42 +0100

    Add layers on LanguageService

--- ui/js/src/main/scala/works/iterative/app/LanguageService.scala
+++ ui/js/src/main/scala/works/iterative/app/LanguageService.scala
@@ -1,5 +1,7 @@
 package works.iterative.app
 
+import zio.*
+
 // Hacky way to manage languages for now, before we switch to something more useful
 trait LanguageService:
     def currentLanguage: String
@@ -10,5 +12,23 @@ object LanguageService:
         override def currentLanguage: String = language
         override def switchLanguage(lang: String) = ()
 
+    def staticLayer(language: String): ZLayer[Any, Nothing, LanguageService] =
+        ZLayer.succeed(static(language))
+
+    def localStorage(appName: String): LanguageService = new LanguageService:
+        override def switchLanguage(lang: String) =
+            Option(org.scalajs.dom.window.localStorage)
+                .foreach(_.setItem(s"${appName}_lang", lang))
+            org.scalajs.dom.window.location.reload()
+        override def currentLanguage: String =
+            Option(org.scalajs.dom.window.localStorage)
+                .flatMap(s => Option(s.getItem(s"${appName}_lang")))
+                .getOrElse("cs")
+
+    def localStorageLayer(appName: String): ZLayer[Any, Nothing, LanguageService] =
+        ZLayer.succeed(localStorage(appName))
+
     val CS: LanguageService = static("cs")
+
+    val CSLayer: ZLayer[Any, Nothing, LanguageService] = ZLayer.succeed(CS)
 end LanguageService
