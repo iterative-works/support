6758d082149f119e959fd81126381bf5a1f3cd57
Michal Příhoda <michal@prihoda.net>
2024-02-28 13:07:45 +0100

    Skrýt záložky Klientovi.

--- core/shared/src/main/scala/works/iterative/core/auth/UserRole.scala
+++ core/shared/src/main/scala/works/iterative/core/auth/UserRole.scala
@@ -4,5 +4,8 @@ package auth
 opaque type UserRole = String
 
 object UserRole extends ValidatedStringFactory[UserRole](r => r):
-  def apply(role: String): Validated[UserRole] =
-    Validated.nonEmptyString("user.role")(role)
+    def apply(role: String): Validated[UserRole] =
+        Validated.nonEmptyString("user.role")(role)
+
+    val Klient = UserRole.unsafe("Klient")
+end UserRole
