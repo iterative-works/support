7bee849529e68ae90f3510ae181009340c705277
Michal Příhoda <michal@prihoda.net>
2024-03-28 22:21:04 +0100

    Context signal support.

--- autocomplete/js/src/main/scala/works/iterative/autocomplete/ui/laminar/AutocompleteQuery.scala
+++ autocomplete/js/src/main/scala/works/iterative/autocomplete/ui/laminar/AutocompleteQuery.scala
@@ -7,10 +7,14 @@ import com.raquo.laminar.api.L.*
 trait AutocompleteQuery:
     def find(q: String): EventStream[List[AutocompleteEntry]]
     def load(id: String): EventStream[Option[AutocompleteEntry]]
+    def withContextSignal(context: Signal[Option[Map[String, String]]]): AutocompleteQuery
+end AutocompleteQuery
 
 object AutocompleteQuery:
     val empty: AutocompleteQuery = new AutocompleteQuery:
         override def find(q: String): EventStream[List[AutocompleteEntry]] = EventStream.empty
-
         override def load(id: String): EventStream[Option[AutocompleteEntry]] = EventStream.empty
+        override def withContextSignal(context: Signal[Option[Map[String, String]]])
+            : AutocompleteQuery =
+            empty
 end AutocompleteQuery
--- autocomplete/js/src/main/scala/works/iterative/autocomplete/ui/laminar/ZIOAutocompleteRegistry.scala
+++ autocomplete/js/src/main/scala/works/iterative/autocomplete/ui/laminar/ZIOAutocompleteRegistry.scala
@@ -15,29 +15,17 @@ class ZIOAutocompleteRegistry(
     mapping: PartialFunction[FieldId, ZIOAutocompleteRegistry.Config],
     context: Option[Map[String, String]] = None
 )(using Runtime[Any]) extends AutocompleteRegistry:
+    import ZIOAutocompleteRegistry.AutocompleteQueryImpl
+
     override def getQueryFor(id: FieldId): Option[AutocompleteQuery] = mapping.lift(id).map: c =>
-        new AutocompleteQuery:
-            val finalContext = (context, c.context) match
-                case (Some(a), Some(b)) => Some(a ++ b)
-                case (Some(a), _)       => Some(a)
-                case (_, b)             => b
+        new AutocompleteQueryImpl(
+            service,
+            languageService,
+            c,
+            context,
+            Val(None)
+        )
 
-            override def find(q: String): EventStream[List[AutocompleteEntry]] =
-                service.find(
-                    c.collection,
-                    q,
-                    c.limit,
-                    languageService.currentLanguage,
-                    finalContext
-                ).map(
-                    _.toList
-                ).toEventStream
-
-            override def load(id: String): EventStream[Option[AutocompleteEntry]] =
-                service.load(c.collection, id, languageService.currentLanguage).map {
-                    case None if !c.strict => Some(AutocompleteEntry(id, id, None, Map.empty))
-                    case c                 => c
-                }.toEventStream
     override def withContext(ctx: Option[Map[String, String]]): AutocompleteRegistry =
         new ZIOAutocompleteRegistry(service, languageService, mapping, ctx)
 end ZIOAutocompleteRegistry
@@ -59,4 +47,43 @@ object ZIOAutocompleteRegistry:
                 given Runtime[Any] <- ZIO.runtime[Any]
             yield ZIOAutocompleteRegistry(service, languageService, mapping)
         }
+
+    def composeContexts(
+        c1: Option[Map[String, String]],
+        c2: Option[Map[String, String]]
+    ): Option[Map[String, String]] = (c1, c2) match
+        case (Some(a), Some(b)) => Some(a ++ b)
+        case (Some(a), _)       => Some(a)
+        case (_, b)             => b
+
+    class AutocompleteQueryImpl(
+        service: AutocompleteService,
+        languageService: LanguageService,
+        config: Config,
+        context: Option[Map[String, String]],
+        additionalContextSignal: Signal[Option[Map[String, String]]]
+    )(using Runtime[Any]) extends AutocompleteQuery:
+        val finalContext = composeContexts(context, config.context)
+
+        override def find(q: String): EventStream[List[AutocompleteEntry]] =
+            additionalContextSignal.flatMap(add =>
+                service.find(
+                    config.collection,
+                    q,
+                    config.limit,
+                    languageService.currentLanguage,
+                    composeContexts(finalContext, add)
+                ).map(_.toList).toEventStream
+            )
+
+        override def load(id: String): EventStream[Option[AutocompleteEntry]] =
+            service.load(config.collection, id, languageService.currentLanguage).map {
+                case None if !config.strict => Some(AutocompleteEntry(id, id, None, Map.empty))
+                case c                      => c
+            }.toEventStream
+
+        override def withContextSignal(ctx: Signal[Option[Map[String, String]]])
+            : AutocompleteQuery =
+            new AutocompleteQueryImpl(service, languageService, config, context, ctx)
+    end AutocompleteQueryImpl
 end ZIOAutocompleteRegistry
