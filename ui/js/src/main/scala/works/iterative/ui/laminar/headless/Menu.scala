96f9234224d9fce2cc168d2a4f4d0b78577c32da
Michal Příhoda <michal@prihoda.net>
2024-05-23 14:48:25 +0200

    Technology update.

--- ui/js/src/main/scala/works/iterative/ui/laminar/headless/Menu.scala
+++ ui/js/src/main/scala/works/iterative/ui/laminar/headless/Menu.scala
@@ -10,66 +10,70 @@ import io.laminext.syntax.core.*
   *
   * The menu wraps existing elements to add the behavior.
   *
-  * It expects menu button, that will toggle the menu state, and show menu
-  * items.
+  * It expects menu button, that will toggle the menu state, and show menu items.
   *
-  * Inspired by headlessui components, but lacks most of the functionality,
-  * especially the ARIA stuff.
+  * Inspired by headlessui components, but lacks most of the functionality, especially the ARIA
+  * stuff.
   */
 // TODO: keyboard support, aria elements, disabled items
 object Menu:
 
-  /** Global menu context */
-  class Ctx:
-    private[Menu] val open: Var[Boolean] = Var(false)
+    /** Global menu context */
+    class Ctx:
+        private[Menu] val open: Var[Boolean] = Var(false)
 
-    val isOpen: Signal[Boolean] = open.signal
-    val close: Observer[Any] = open.writer.contramap(_ => false)
+        val isOpen: Signal[Boolean] = open.signal
+        val close: Observer[Any] = open.writer.contramap(_ => false)
+    end Ctx
 
-  class ItemCtx(using ctx: Ctx):
-    private[Menu] val active: Var[Boolean] = Var(false)
-    val isActive: Signal[Boolean] = active.signal
-    export ctx.close
+    class ItemCtx(using ctx: Ctx):
+        private[Menu] val active: Var[Boolean] = Var(false)
+        val isActive: Signal[Boolean] = active.signal
+        export ctx.close
+    end ItemCtx
 
-  type Render = Ctx ?=> HtmlElement
-  type ItemRender = ItemCtx ?=> HtmlElement
+    type Render = Ctx ?=> HtmlElement
+    type ItemRender = ItemCtx ?=> HtmlElement
 
-  def apply(
-      content: Render
-  ): HtmlElement =
-    given ctx: Ctx = Ctx()
-    content.amend(closeOnClickOutside(ctx.open))
+    def apply(
+        content: Render
+    ): HtmlElement =
+        given ctx: Ctx = Ctx()
+        content.amend(closeOnClickOutside(ctx.open))
+    end apply
 
-  def button(but: Render)(using ctx: Ctx): HtmlElement =
-    but.amend(
-      aria.expanded <-- ctx.isOpen,
-      dataState <-- ctx.isOpen.map {
-        case true  => "open"
-        case false => ""
-      },
-      onClick.mapTo(true) --> ctx.open.writer
-    )
+    def button(but: Render)(using ctx: Ctx): HtmlElement =
+        but.amend(
+            aria.expanded <-- ctx.isOpen,
+            dataState <-- ctx.isOpen.map {
+                case true  => "open"
+                case false => ""
+            },
+            onClick.mapTo(true) --> ctx.open.writer
+        )
 
-  def items(el: Render)(using ctx: Ctx): HtmlElement =
-    el.amend(
-      cls.toggle("hidden") <-- ctx.isOpen.not,
-      dataState <-- ctx.isOpen.map {
-        case true  => "open"
-        case false => ""
-      }
-    )
+    def items(el: Render)(using ctx: Ctx): HtmlElement =
+        el.amend(
+            cls("hidden") <-- ctx.isOpen.not,
+            dataState <-- ctx.isOpen.map {
+                case true  => "open"
+                case false => ""
+            }
+        )
 
-  def item(it: ItemRender)(using
-      ctx: Ctx
-  ): HtmlElement =
-    given ictx: ItemCtx = ItemCtx()
+    def item(it: ItemRender)(using
+        ctx: Ctx
+    ): HtmlElement =
+        given ictx: ItemCtx = ItemCtx()
 
-    it.amend(
-      onMouseEnter.mapTo(true) --> ictx.active.writer,
-      onMouseLeave.mapTo(false) --> ictx.active.writer,
-      onClick.mapTo(false) --> ctx.open.writer,
-      dataState <-- ictx.isActive.map {
-        case true  => "active"
-        case false => ""
-      }
-    )
+        it.amend(
+            onMouseEnter.mapTo(true) --> ictx.active.writer,
+            onMouseLeave.mapTo(false) --> ictx.active.writer,
+            onClick.mapTo(false) --> ctx.open.writer,
+            dataState <-- ictx.isActive.map {
+                case true  => "active"
+                case false => ""
+            }
+        )
+    end item
+end Menu
