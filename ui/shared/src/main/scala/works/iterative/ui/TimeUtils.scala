31a9e2bc66a3304284fdfc1a665c0b9fa9599573
Michal Příhoda <michal@prihoda.net>
2024-08-30 12:30:30 +0200

    Add parser for HTML dates

--- ui/shared/src/main/scala/works/iterative/ui/TimeUtils.scala
+++ ui/shared/src/main/scala/works/iterative/ui/TimeUtils.scala
@@ -4,40 +4,49 @@ import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 import java.time.ZoneId
 import java.time.temporal.TemporalAccessor
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 object TimeUtils:
-  val dateTimeFormat =
-    DateTimeFormatter
-      .ofLocalizedDateTime(FormatStyle.SHORT)
-      // TODO: locale
-      // .withLocale(Locale("cs", "CZ"))
-      .withZone(ZoneId.systemDefault())
+    val dateTimeFormat =
+        DateTimeFormatter
+            .ofLocalizedDateTime(FormatStyle.SHORT)
+            // TODO: locale
+            // .withLocale(Locale("cs", "CZ"))
+            .withZone(ZoneId.systemDefault())
 
-  val dateFormat =
-    DateTimeFormatter
-      .ofLocalizedDate(FormatStyle.SHORT)
-      // TODO: locale
-      // .withLocale(Locale("cs", "CZ"))
-      .withZone(ZoneId.systemDefault())
+    val dateFormat =
+        DateTimeFormatter
+            .ofLocalizedDate(FormatStyle.SHORT)
+            // TODO: locale
+            // .withLocale(Locale("cs", "CZ"))
+            .withZone(ZoneId.systemDefault())
 
-  val htmlDateFormat =
-    DateTimeFormatter
-      .ofPattern("yyyy-MM-dd")
-      .withZone(ZoneId.systemDefault())
+    val htmlDateFormat =
+        DateTimeFormatter
+            .ofPattern("yyyy-MM-dd")
+            .withZone(ZoneId.systemDefault())
 
-  val htmlDateTimeFormat =
-    DateTimeFormatter
-      .ofPattern("yyyy-MM-dd HH:mm:ss")
-      .withZone(ZoneId.systemDefault())
+    val htmlDateTimeFormat =
+        DateTimeFormatter
+            .ofPattern("yyyy-MM-dd HH:mm:ss")
+            .withZone(ZoneId.systemDefault())
 
-  def formatDateTime(i: TemporalAccessor): String =
-    dateTimeFormat.format(i)
+    def formatDateTime(i: TemporalAccessor): String =
+        dateTimeFormat.format(i)
 
-  def formatDate(i: TemporalAccessor): String =
-    dateFormat.format(i)
+    def formatDate(i: TemporalAccessor): String =
+        dateFormat.format(i)
 
-  def formatHtmlDate(i: TemporalAccessor): String =
-    htmlDateFormat.format(i)
+    def formatHtmlDate(i: TemporalAccessor): String =
+        htmlDateFormat.format(i)
 
-  def formatHtmlDateTime(i: TemporalAccessor): String =
-    htmlDateTimeFormat.format(i)
+    def formatHtmlDateTime(i: TemporalAccessor): String =
+        htmlDateTimeFormat.format(i)
+
+    def parseHtmlLocalDate(i: String): LocalDate =
+        LocalDate.parse(i, htmlDateFormat)
+
+    def parseHtmlLocalDateTime(i: String): LocalDateTime =
+        LocalDateTime.parse(i, htmlDateTimeFormat)
+end TimeUtils
