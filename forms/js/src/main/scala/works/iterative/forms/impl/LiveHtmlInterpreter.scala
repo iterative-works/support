b09e2d5acf5ee98e19b7a119690a5c1bc283821d
Michal Příhoda <michal@prihoda.net>
2024-09-30 20:06:39 +0200

    Fix form rendering

--- forms/js/src/main/scala/works/iterative/forms/impl/LiveHtmlInterpreter.scala
+++ forms/js/src/main/scala/works/iterative/forms/impl/LiveHtmlInterpreter.scala
@@ -348,7 +348,17 @@ class LiveHtmlInterpreter(
         def cont(i: SectionSegment) = renderSegment(i)
         hooks.aroundSection(id): fi =>
             val fullId = fi.id / id
-            val content = FormPart.combine(id, elems.map(cont)*)(cs.flexRow)
+            val layout = layoutResolver.resolve(fullId, elems)
+            val content = layout match
+                case Grid(elems) =>
+                    FormPart.combine(
+                        id,
+                        elems.flatMap(segments =>
+                            segments.map(cont(_).mapDom(cs.gridCell(segments.size, _)))
+                        )*
+                    )(cs.grid)
+                case Flex(elems) =>
+                    FormPart.combine(id, elems.map(cont)*)(cs.flexRow)
 
             val unknown = ValidationState.Unknown(
                 List(fi.id -> UserMessage("error.validation.unknown", fi.id.toHtmlId))
