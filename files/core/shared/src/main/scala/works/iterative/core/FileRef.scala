40260b6739772e266ac5fb3654ded3709fa73122
Michal Příhoda <michal@prihoda.net>
2024-10-01 16:27:24 +0200

    Fix double slash in file ref

--- files/core/shared/src/main/scala/works/iterative/core/FileRef.scala
+++ files/core/shared/src/main/scala/works/iterative/core/FileRef.scala
@@ -8,48 +8,54 @@ case class FileRef(
     url: String,
     metadata: FileStore.Metadata
 ):
-  def fileType = metadata.get(FileStore.Metadata.FileType)
-  def size = metadata.get(FileStore.Metadata.Size).map(_.toLong)
+    def fileType = metadata.get(FileStore.Metadata.FileType)
+    def size = metadata.get(FileStore.Metadata.Size).map(_.toLong)
 
-  def sizeString: Option[String] =
-    size.map {
-      case s if s < 1024               => s"$s B"
-      case s if s < 1024 * 1024        => s"${s / 1024} KB"
-      case s if s < 1024 * 1024 * 1024 => s"${s / (1024 * 1024)} MB"
-      case s                           => s"${s / (1024 * 1024 * 1024)} GB"
-    }
+    def sizeString: Option[String] =
+        size.map {
+            case s if s < 1024               => s"$s B"
+            case s if s < 1024 * 1024        => s"${s / 1024} KB"
+            case s if s < 1024 * 1024 * 1024 => s"${s / (1024 * 1024)} MB"
+            case s                           => s"${s / (1024 * 1024 * 1024)} GB"
+        }
 
-  def withBase(url: String): FileRef = copy(url = url + this.url)
+    def withBase(url: String): FileRef =
+        val normalizedUrl = if url.endsWith("/") then url else url + "/"
+        val normalizedFileUrl = if this.url.startsWith("/") then this.url.drop(1) else this.url
+        copy(url = normalizedUrl + normalizedFileUrl)
+    end withBase
+end FileRef
 
 object FileRef:
-  import FileStore.Metadata
+    import FileStore.Metadata
 
-  private def metadata(
-      fileType: Option[String] = None,
-      size: Option[Long] = None
-  ): Metadata =
-    List(
-      fileType.map(Metadata.FileType -> _),
-      size.map(Metadata.Size -> _.toString)
-    ).flatten.toMap
+    private def metadata(
+        fileType: Option[String] = None,
+        size: Option[Long] = None
+    ): Metadata =
+        List(
+            fileType.map(Metadata.FileType -> _),
+            size.map(Metadata.Size -> _.toString)
+        ).flatten.toMap
 
-  def apply(
-      name: String,
-      url: String,
-      fileType: Option[String] = None,
-      size: Option[Long] = None
-  ): Validated[FileRef] =
-    for
-      name <- Validated.nonEmptyString("file.name")(name)
-      url <- Validated.nonEmptyString("file.url")(url)
-    yield new FileRef(name, url, metadata(fileType, size))
+    def apply(
+        name: String,
+        url: String,
+        fileType: Option[String] = None,
+        size: Option[Long] = None
+    ): Validated[FileRef] =
+        for
+            name <- Validated.nonEmptyString("file.name")(name)
+            url <- Validated.nonEmptyString("file.url")(url)
+        yield new FileRef(name, url, metadata(fileType, size))
 
-  def unsafe(
-      name: String,
-      url: String,
-      fileType: Option[String] = None,
-      size: Option[Long] = None
-  ): FileRef = new FileRef(name, url, metadata(fileType, size))
+    def unsafe(
+        name: String,
+        url: String,
+        fileType: Option[String] = None,
+        size: Option[Long] = None
+    ): FileRef = new FileRef(name, url, metadata(fileType, size))
 
-  def unsafe(name: String, url: String, metadata: Metadata) =
-    new FileRef(name, url, metadata)
+    def unsafe(name: String, url: String, metadata: Metadata) =
+        new FileRef(name, url, metadata)
+end FileRef
