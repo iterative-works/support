6c5625e5f443b17ac355d0185d34f400ffafc9dd
Michal Příhoda <michal@prihoda.net>
2024-12-04 13:53:57 +0100

    Remove Cmi-specific resolvers

--- forms/js/src/main/scala/works/iterative/forms/impl/DisplayResolver.scala
+++ forms/js/src/main/scala/works/iterative/forms/impl/DisplayResolver.scala
@@ -2,13 +2,11 @@ package portaly
 package forms
 package impl
 
-import zio.*
 import com.raquo.laminar.api.L.*
 import works.iterative.ui.model.forms.IdPath
 import works.iterative.ui.model.forms.FormState
 import works.iterative.core.MessageCatalogue
 import works.iterative.core.Language
-import works.iterative.core.UserMessage
 
 trait LiveHtmlDisplayResolver extends DisplayResolver[FormCtx, HtmlElement]:
     override def resolve(id: IdPath, ctx: FormCtx)(using MessageCatalogue, Language): HtmlElement
@@ -19,64 +17,3 @@ trait ReadOnlyHtmlDisplayResolver extends DisplayResolver[FormState, HtmlElement
         Language
     ): HtmlElement
 end ReadOnlyHtmlDisplayResolver
-
-class CmiLiveDisplayResolver extends LiveHtmlDisplayResolver:
-    override def resolve(id: IdPath, ctx: FormCtx)(using MessageCatalogue, Language): HtmlElement =
-        id.last match
-            case "metpokyn_display" =>
-                div(
-                    child <-- ctx.state.get(id.up / "metpokyn").map:
-                        case Some(v: String) if !v.isBlank() =>
-                            div(
-                                label(
-                                    cls(
-                                        "block text-sm font-medium leading-6 text-neutral-700"
-                                    ),
-                                    UserMessage("meridlo.metpokyn.label").asString
-                                ),
-                                div(
-                                    cls("mt-1 text-sm text-neutral-900"),
-                                    v
-                                )
-                            )
-                        case _ => div()
-                )
-            case _ => div()
-end CmiLiveDisplayResolver
-
-class CmiReadOnlyDisplayResolver extends ReadOnlyHtmlDisplayResolver:
-    override def resolve(id: IdPath, state: FormState)(using
-        MessageCatalogue,
-        Language
-    ): HtmlElement =
-        id.last match
-            case "metpokyn_display" =>
-                div(
-                    state.getString(IdPath.full((id.up / "metpokyn").serialize)).map: v =>
-                        if !v.isBlank() then
-                            div(
-                                label(
-                                    cls(
-                                        "block text-sm font-medium leading-6 text-neutral-700"
-                                    ),
-                                    UserMessage("meridlo.metpokyn.label").asString
-                                ),
-                                div(
-                                    cls("mt-1 text-sm text-neutral-900"),
-                                    v
-                                )
-                            )
-                        else div()
-                )
-            case _ => div()
-end CmiReadOnlyDisplayResolver
-
-object DefaultDisplayResolvers:
-    val layer: ULayer[LiveHtmlDisplayResolver & ReadOnlyHtmlDisplayResolver] =
-        ZLayer.succeedEnvironment(
-            ZEnvironment[LiveHtmlDisplayResolver, ReadOnlyHtmlDisplayResolver](
-                CmiLiveDisplayResolver(),
-                CmiReadOnlyDisplayResolver()
-            )
-        )
-end DefaultDisplayResolvers
