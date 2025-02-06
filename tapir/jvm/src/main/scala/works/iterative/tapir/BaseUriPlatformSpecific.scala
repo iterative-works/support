0799fe8c72aa9e247d95c9e22d4a35724b6fdfd3
Michal Příhoda <michal@prihoda.net>
2025-02-06 23:19:50 +0100

    Add fromConfig BaseUri layer

--- tapir/jvm/src/main/scala/works/iterative/tapir/BaseUriPlatformSpecific.scala
+++ tapir/jvm/src/main/scala/works/iterative/tapir/BaseUriPlatformSpecific.scala
@@ -6,4 +6,6 @@ import sttp.model.Uri.*
 trait BaseUriPlatformSpecific:
     given config: Config[BaseUri] =
         Config.string("baseuri").optional.map(_.map(s => uri"$s")).map(BaseUri.apply)
+
+    def fromConfig: ZLayer[Any, Config.Error, BaseUri] = ZLayer(ZIO.config[BaseUri](config))
 end BaseUriPlatformSpecific
