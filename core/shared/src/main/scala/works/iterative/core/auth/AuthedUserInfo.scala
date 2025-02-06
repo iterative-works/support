e079cbd0f5f36c0d634ea1d7f172caec0ccb945e
Michal Příhoda <michal@prihoda.net>
2025-02-06 23:20:24 +0100

    Make AuthedUserInfo a UserInfo

--- core/shared/src/main/scala/works/iterative/core/auth/AuthedUserInfo.scala
+++ core/shared/src/main/scala/works/iterative/core/auth/AuthedUserInfo.scala
@@ -2,4 +2,5 @@ package works.iterative.core.auth
 
 case class AccessToken(token: String)
 
-case class AuthedUserInfo(token: AccessToken, profile: BasicProfile)
+case class AuthedUserInfo(token: AccessToken, profile: BasicProfile) extends UserProfile:
+    export profile.*
