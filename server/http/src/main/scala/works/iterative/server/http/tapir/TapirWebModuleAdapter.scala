ea55536644714364e207abd5d3deb7ce24702e43
Michal Příhoda <michal@prihoda.net>
2025-05-19 10:18:56 +0200

    Add capabilities type parameter to TapirEndpointModule

--- server/http/src/main/scala/works/iterative/server/http/tapir/TapirWebModuleAdapter.scala
+++ server/http/src/main/scala/works/iterative/server/http/tapir/TapirWebModuleAdapter.scala
@@ -6,22 +6,23 @@ import zio.RIO
 import org.http4s.HttpRoutes
 import works.iterative.server.http.WebFeatureModule
 import zio.interop.catz.*
+import sttp.capabilities.zio.ZioStreams
 
 /** Adapts a TapirEndpointModule to a WebFeatureModule with HTTP routes */
 object TapirWebModuleAdapter:
     /** Converts a TapirEndpointModule to a WebFeatureModule */
-    def adapt[R](
+    def adapt[R, C >: ZioStreams](
         options: Http4sServerOptions[RIO[R, *]] = Http4sServerOptions.default,
-        module: TapirEndpointModule[R]
+        module: TapirEndpointModule[R, C]
     ): WebFeatureModule[RIO[R, *]] =
         new WebFeatureModule[RIO[R, *]]:
             override def routes: HttpRoutes[RIO[R, *]] =
                 ZHttp4sServerInterpreter(options).from(module.serverEndpoints).toRoutes
 
     /** Combines multiple TapirEndpointModules and converts them to a single WebFeatureModule */
-    def combine[R](
+    def combine[R, C >: ZioStreams](
         options: Http4sServerOptions[RIO[R, *]] = Http4sServerOptions.default,
-        modules: TapirEndpointModule[R]*
+        modules: TapirEndpointModule[R, C]*
     ): WebFeatureModule[RIO[R, *]] =
         adapt(options, TapirEndpointModule.combine(modules*))
 end TapirWebModuleAdapter
