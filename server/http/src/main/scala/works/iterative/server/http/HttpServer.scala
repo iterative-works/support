cac837916fe36123b3ecb0d386b5c8ce57c565bb
Michal Příhoda <michal@prihoda.net>
2025-06-29 11:05:52 +0200

    Add build method to HttpServer and implement in BlazeHttpServer

--- server/http/src/main/scala/works/iterative/server/http/HttpServer.scala
+++ server/http/src/main/scala/works/iterative/server/http/HttpServer.scala
@@ -5,11 +5,20 @@ import org.http4s.HttpRoutes
 import org.http4s.server.websocket.WebSocketBuilder2
 
 trait HttpServer:
+    def build[Env](
+        app: WebSocketBuilder2[RIO[Env, *]] => HttpRoutes[RIO[Env, *]]
+    ): RIO[Env & Scope, org.http4s.server.Server]
+
     def serve[Env](app: WebSocketBuilder2[RIO[Env, *]] => HttpRoutes[RIO[Env, *]])
         : URIO[Env, Nothing]
 end HttpServer
 
 object HttpServer:
+    def build[Env](
+        app: WebSocketBuilder2[RIO[Env, *]] => HttpRoutes[RIO[Env, *]]
+    ): RIO[Env & Scope & HttpServer, org.http4s.server.Server] =
+        ZIO.serviceWithZIO[HttpServer](_.build(app))
+
     def serve[Env](
         app: WebSocketBuilder2[RIO[Env, *]] => HttpRoutes[RIO[Env, *]]
     ): URIO[Env & HttpServer, Nothing] =
--- server/http/src/main/scala/works/iterative/server/http/impl/blaze/BlazeHttpServer.scala
+++ server/http/src/main/scala/works/iterative/server/http/impl/blaze/BlazeHttpServer.scala
@@ -6,21 +6,42 @@ import zio.interop.catz.*
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.*
 import cats.*
+import zio.interop.catz.*
 import works.iterative.tapir.BaseUri
 import org.http4s.server.Router
 import org.http4s.server.websocket.WebSocketBuilder2
 
 class BlazeHttpServer(config: BlazeServerConfig, baseUri: BaseUri) extends HttpServer:
+    def withBaseUri[Env](routes: HttpRoutes[RIO[Env, *]]): HttpRoutes[RIO[Env, *]] =
+        baseUri.value match
+            case Some(u) => Router(u.toString -> routes)
+            case _       => routes
+
+    override def build[Env](
+        httpApp: WebSocketBuilder2[RIO[Env, *]] => HttpRoutes[RIO[Env, *]]
+    ): RIO[Env & Scope, org.http4s.server.Server] =
+        for
+            _ <- ZIO.log(s"Starting Blaze server with config: $config")
+            executor <- ZIO.executor
+            server <- BlazeServerBuilder[RIO[Env, *]]
+                .withExecutionContext(executor.asExecutionContext)
+                .bindHttp(config.port, config.host)
+                .withResponseHeaderTimeout(config.responseHeaderTimeout.asScala)
+                .withIdleTimeout(config.idleTimeout.asScala)
+                .withHttpWebSocketApp(wsb => withBaseUri(httpApp(wsb)).orNotFound)
+                .resource
+                .toScopedZIO
+        yield server
+    end build
+
     override def serve[Env](
         httpApp: WebSocketBuilder2[RIO[Env, *]] => HttpRoutes[RIO[Env, *]]
     ): URIO[Env, Nothing] =
-        def withBaseUri(routes: HttpRoutes[RIO[Env, *]]): HttpRoutes[RIO[Env, *]] =
-            baseUri.value match
-                case Some(u) => Router(u.toString -> routes)
-                case _       => routes
-
-        ZIO.log(s"Starting Blaze server with config: $config") *>
-            BlazeServerBuilder[RIO[Env, *]]
+        for
+            _ <- ZIO.log(s"Starting Blaze server with config: $config")
+            executor <- ZIO.executor
+            _ <- BlazeServerBuilder[RIO[Env, *]]
+                .withExecutionContext(executor.asExecutionContext)
                 .bindHttp(config.port, config.host)
                 .withResponseHeaderTimeout(config.responseHeaderTimeout.asScala)
                 .withIdleTimeout(config.idleTimeout.asScala)
@@ -28,7 +49,9 @@ class BlazeHttpServer(config: BlazeServerConfig, baseUri: BaseUri) extends HttpS
                 .serve
                 .compile
                 .drain
-                .orDie *> ZIO.never
+                .orDie
+            result <- ZIO.never
+        yield result
     end serve
 end BlazeHttpServer
 
